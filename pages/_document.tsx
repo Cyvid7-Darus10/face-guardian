b92a1bfd1cd008056463e3e30a1a3250ea377b71
Cyrus David Pastelero <cgpastelero@up.edu.ph>
2023-03-02 03:30:11 +0800

    feat(FG-4): Improved the site's SEO friendliness

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,37 @@ export default function Document() {
 	return (
 		<Html lang="en">
 			<Head>
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<meta
+					name="description"
+					content="Facial authentication OAuth provider for secure and seamless authentication."
+				/>
+				<meta
+					name="keywords"
+					content="facial authentication, OAuth provider, secure authentication"
+				/>
+				<link rel="canonical" href="https://www.face-guardian.com/" />
+				<meta
+					property="og:title"
+					content="Facial Authentication OAuth Provider - Face Guardian"
+				/>
+				<meta
+					property="og:description"
+					content="Facial authentication OAuth provider for secure and seamless authentication."
+				/>
+				<meta property="og:type" content="website" />
+				<meta property="og:url" content="https://www.face-guardian.com/" />
+				<meta property="og:image" content="/fg-logo.png" />
+				<meta property="twitter:card" content="summary_large_image" />
+				<meta
+					property="twitter:title"
+					content="Facial Authentication OAuth Provider - Face Guardian"
+				/>
+				<meta
+					property="twitter:description"
+					content="Facial authentication OAuth provider for secure and seamless authentication."
+				/>
+				<meta property="twitter:image" content="/fg-logo.png" />
 				<link
 					rel="apple-touch-icon"
 					sizes="180x180"
