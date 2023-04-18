b3d66785274b383f2dfb94431f4ed0feff57a1bb
Cyrus David Pastelero <cgpastelero@up.edu.ph>
2023-04-19 00:43:56 +0800

    fix: Removed the service worker

--- next.config.js
+++ next.config.js
@@ -1,29 +1,6 @@
-const { InjectManifest } = require("workbox-webpack-plugin");
-
-module.exports = {
-	webpack(config, { isServer, dev }) {
-		if (!isServer && !dev) {
-			config.plugins.push(
-				new InjectManifest({
-					swSrc: "./public/service-worker.js",
-					swDest: "./service-worker.js",
-					exclude: [/\.map$/, /asset-manifest\.json$/],
-				})
-			);
-		}
-		return config;
-	},
-	async headers() {
-		return [
-			{
-				source: "/service-worker.js",
-				headers: [
-					{
-						key: "Cache-Control",
-						value: "public, max-age=0, must-revalidate",
-					},
-				],
-			},
-		];
-	},
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	reactStrictMode: false,
 };
+
+module.exports = nextConfig;
