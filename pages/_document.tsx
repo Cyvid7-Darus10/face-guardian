953d828ad022048baa3597ace48f44a10d61148f
Cyrus David Pastelero <cgpastelero@up.edu.ph>
2023-04-19 01:15:16 +0800

    feat: Added PWA

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,20 @@ export default function Document() {
 	return (
 		<Html lang="en">
 			<Head>
+				<meta name="application-name" content="Face Guardian" />
+				<meta name="apple-mobile-web-app-capable" content="yes" />
+				<meta name="apple-mobile-web-app-status-bar-style" content="default" />
+				<meta name="apple-mobile-web-app-title" content="Face Guardian" />
+				<meta name="description" content="Face Guardian" />
+				<meta name="format-detection" content="telephone=no" />
+				<meta name="mobile-web-app-capable" content="yes" />
+				<meta
+					name="msapplication-config"
+					content="/favicons/favicon-32x32.png"
+				/>
+				<meta name="msapplication-TileColor" content="#2B5797" />
+				<meta name="msapplication-tap-highlight" content="no" />
+				<meta name="theme-color" content="#000000" />
 				<link
 					rel="apple-touch-icon"
 					sizes="180x180"
