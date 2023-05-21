fd00bd8b650d2bb3d50c4e5e2d64eeb74231d00f
Cyrus David Pastelero <cgpastelero@up.edu.ph>
2023-05-21 18:57:39 +0800

    chore: Updated the authenticate api to allow other access to api

--- pages/api/authenticate.ts
+++ pages/api/authenticate.ts
@@ -6,6 +6,21 @@ import { createClient } from "@supabase/supabase-js";
 export default withIronSessionApiRoute(authenticateRoute, sessionOptions);
 
 async function authenticateRoute(req: NextApiRequest, res: NextApiResponse) {
+	res.setHeader("Access-Control-Allow-Credentials", "true");
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader(
+		"Access-Control-Allow-Methods",
+		"GET,OPTIONS,PATCH,DELETE,POST,PUT"
+	);
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+	);
+
+	if (req.method === "OPTIONS") {
+		return res.status(200).end();
+	}
+
 	const { appId, clientSecret } = req.body;
 	const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
 	const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
