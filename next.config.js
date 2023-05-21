491ab86f41a7edff28a5813bfbe061c2d170f80d
Cyrus David Pastelero <cgpastelero@up.edu.ph>
2023-05-21 19:07:14 +0800

    chore: Removed configs for cors and added vercel.json instead

--- next.config.js
+++ next.config.js
@@ -3,27 +3,8 @@ const withPWA = require("next-pwa")({
 	dest: "public",
 });
 
-module.exports = withPWA({
+const nextConfig = {
 	reactStrictMode: false,
-	headers: async () => {
-		return [
-			{
-				// matching all API routes
-				source: "/api/:path*",
-				headers: [
-					{ key: "Access-Control-Allow-Credentials", value: "true" },
-					{ key: "Access-Control-Allow-Origin", value: "*" },
-					{
-						key: "Access-Control-Allow-Methods",
-						value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-					},
-					{
-						key: "Access-Control-Allow-Headers",
-						value:
-							"X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-					},
-				],
-			},
-		];
-	},
-});
+};
+
+module.exports = withPWA(nextConfig);
