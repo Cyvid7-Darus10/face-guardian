04eea2b2ee63ed0875539bc223c1e9ce97bc877a
Cyrus David Pastelero <cgpastelero@up.edu.ph>
2023-05-22 02:35:17 +0800

    feat: Updated the CORS policy

--- next.config.js
+++ next.config.js
@@ -20,7 +20,7 @@ module.exports = withPWA({
 					{
 						key: "Access-Control-Allow-Headers",
 						value:
-							"X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+							"X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization",
 					},
 				],
 			},
